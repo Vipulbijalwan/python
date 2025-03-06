9db88b22b5d5d5c9c5eef09c216582ab6d80f149
vipul <vipulbijalwan112@gmail.com>
2025-03-06 21:24:06 +0530

    learning PANDAS

---
+++ BASIC OF PANDAS/12) Handling Missing Data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data={
+    "Name":["TOM",None,"JHON","HARRY","BARRY","LARRY","GARRY","TERRY"],
+
+    "Age":[25,None,47,22,23,45,78,99],
+    "Salary":[45000,None,67000,34000,56000,78000,55000,60000]
+}
+
+df=pd.DataFrame(data)
+ 
+print(df)
+
+print(df.isnull().sum())
