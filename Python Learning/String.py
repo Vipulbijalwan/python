701fd430bd5b21d37bd65dd5892c9bdf0c65df58
vipul <vipulbijalwan112@gmail.com>
2025-03-07 21:25:37 +0530

    Python

---
+++ Python Learning/String.py
@@ -0,0 +1,5 @@
+text="hello everyone welcome to python programming"
+
+print(text[0:20:2]) #slice operation
+
+print('hello' in text)
---
+++ Python Learning/conditional.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age:"))
+
+if age>=18:
+    print("you")
+else:
+    print('not you')
+        
